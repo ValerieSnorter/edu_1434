22b2483a2d394882c0767797534d1d54091d5fe3
ValerieSnorter <74143687+ValerieSnorter@users.noreply.github.com>
2020-11-08 22:36:36 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+   
+ public  static  void  main ( String [] args ) {
+        String firstname = "Олег" ;
+        String city = "Омск" ;
+        int age = 16;
+        System.out.println(firstname+" "+age+" "+"лет"+" "+city);
+    }
+}
