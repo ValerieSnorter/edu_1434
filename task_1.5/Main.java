84cfed8dfc375679d45453425625063bfc5eb2c7
ValerieSnorter <74143687+ValerieSnorter@users.noreply.github.com>
2020-11-08 23:08:03 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,17 @@
+Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
+TF = (9 / 5) * TC + 32
+Пример:
+на вход подается значение 41.
+Пример вывода:
+105.8 */
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int a = scan.nextInt();
+      double b = 1.8;
+      System.out.println((b*a) + 32);
+        
+    }
+}
