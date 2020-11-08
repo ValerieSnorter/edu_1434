7d672b39796b71a2d2e318758ef371b14d05f0f3
ValerieSnorter <74143687+ValerieSnorter@users.noreply.github.com>
2020-11-08 22:55:21 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,13 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int namber = scan.nextInt();
+      int c = (namber*namber);
+      if (namber==25) System.out.println(c);
+        
+    }
+}
