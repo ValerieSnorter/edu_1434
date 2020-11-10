3615fbb70addc6840234d6e64a0779c06be8d9ae
ValerieSnorter <74143687+ValerieSnorter@users.noreply.github.com>
2020-11-10 13:05:54 +0300

    Create Main.java

---
+++ task_2.4/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int a = scan.nextInt();
+      double b = scan.nextDouble();
+      double c =(a*(b/100));
+   // цикл
+System.out.println (c); }
