261a7823942ae37d3928cac5e81c159512573be4
ValerieSnorter <74143687+ValerieSnorter@users.noreply.github.com>
2020-11-11 14:14:56 +0300

    Update and rename task_2.2/Main.java to task_2.3/Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	 public static void main(String[] args) {
+        for(int a = 0; a < 11; a++){
+        for (int b = a; b > 0; b--){
+        System.out.print("8");
+        }
+         System.out.println();
+        }
+    }
+}
+          
+       
